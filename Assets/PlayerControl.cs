77d85eb80a16194350fea80b6e5ea46711a994aa
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-06-02 10:28:20 -0300

    Initial commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            transform.Translate(speed , 0 , 0);
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(-speed , 0 , 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+       {
+        transform.eulerAngles += new Vector3(0 , rotationSpeed , 0);
+       }
+        
+        if (Input.GetKey(KeyCode.A))
+       {
+        transform.eulerAngles -= new Vector3(0 , rotationSpeed , 0);
+       }
+    }
+}
