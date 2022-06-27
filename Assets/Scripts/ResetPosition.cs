502a020de820d0f9c98341a2c32b5c24d543654c
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-06-27 20:06:59 -0300

    :top:

---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision piso)
+    {
+        if (piso.gameObject.name == "piso")
+        {
+            transform.position = new Vector3(-73.76f, 18, -0.24f);
+        }
+    }
+}
